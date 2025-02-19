6f99ba3912d75f9c085efd7a3e2a240b9d383987
bancerk <192424850+bancerk@users.noreply.github.com>
2025-02-19 11:55:25 +0300

    Kodlamaya Isınma Egzersizi - 4

---
+++ kodlamaya-isinma-egzersizi-4/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+    for(int x=1; x<=50; x++){
+    if (x % 4 == 0) {
+        System.out.println(x + " 4'ün katıdır.");
+    }
+    if (x % 3 == 0) {
+        System.out.println(x + " 3'ün katıdır.");
+            }
+    if (x % 3 == 0){
+        if (x % 4 == 0) {
+        System.out.println(x + " hem 3'ün hem de 4'ün katıdır.");
+                }
+            }
+        }
+    }
+}
