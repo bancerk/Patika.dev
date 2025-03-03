d1b101b33d006b2023d6ffdf3fb2f2834dcdf100
bancerk <192424850+bancerk@users.noreply.github.com>
2025-03-03 15:39:58 +0300

    syntax fix

--- 1-hafta/src/main.java
+++ 1-hafta/src/main.java
@@ -34,10 +34,10 @@ public class main {
         int not = 85;
         String harfNotu;
 
-        if (not>=85){harfNotu="A"};
-        else if (not>=70){harfNotu="B"};
-        else if (not>=60){harfNotu="C"};
-        else {harfNotu="D"};
+        if (not>=85){harfNotu="A";}
+        else if (not>=70){harfNotu="B";}
+        else if (not>=60){harfNotu="C";}
+        else {harfNotu="D";}
 
         System.out.println(harfNotu);
     }
