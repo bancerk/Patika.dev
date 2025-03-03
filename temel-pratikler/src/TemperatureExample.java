a95856a44ebfd18ebecec9e97f8e2d8f6b927036
bancerk <192424850+bancerk@users.noreply.github.com>
2025-03-03 11:28:40 +0300

    temel pratikler

---
+++ temel-pratikler/src/TemperatureExample.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TemperatureExample {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Celsius cinsinden sıcaklık girin: ");
+
+        double celsius = scanner.nextDouble();
+
+        // F = (C * 1.8) + 32
+
+        double f = (celsius * 1.8) + 32;
+
+        System.out.println("C -> F : " + f);
+    }
+}
