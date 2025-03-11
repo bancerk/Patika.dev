3afd880d354b2e777ce00a175ae329baeae6f88e
bancerk <192424850+bancerk@users.noreply.github.com>
2025-03-11 14:31:33 +0300

    comments

--- 2-hafta/src/Odevler/SayiYuvarlama.java
+++ 2-hafta/src/Odevler/SayiYuvarlama.java
@@ -6,20 +6,27 @@ public class SayiYuvarlama {
 
     public static void main(String[] args) {
 
+        // Kullanıcıdan giriş almak için Scanner nesnesi oluşturuluyor
         Scanner scanner = new Scanner(System.in);
 
+        // Kullanıcıdan bir ondalıklı sayı girmesini istiyoruz
         System.out.println("Bir ondalık sayı giriniz: ");
 
+        // Kullanıcının girdiği ondalıklı sayı değişkene atanıyor
         double userInput = scanner.nextDouble();
 
+        // Sayıyı aşağı yuvarlayarak ekrana yazdırıyoruz
         System.out.println("Aşağı Yuvarlama: " + (int) Math.floor(userInput));
 
+        // Sayıyı yukarı yuvarlayarak ekrana yazdırıyoruz
         System.out.println("Yukarı yuvarlama: " + (int) Math.ceil(userInput));
 
+        // Sayıyı en yakın tam sayıya yuvarlayarak ekrana yazdırıyoruz
         System.out.println("En Yakın Tam Sayı: " + Math.round(userInput));
 
     }
 }
+
 
 /* Input: 5.7
 
