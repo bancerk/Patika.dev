df95337ea6b6a64905faae8596ca27b879efae0f
bancerk <192424850+bancerk@users.noreply.github.com>
2025-03-11 17:12:23 +0300

    methods homework

--- 2-hafta/src/Odevler/GecerliSifreKontrolu.java
+++ 2-hafta/src/Odevler/GecerliSifreKontrolu.java
@@ -6,24 +6,37 @@ public class GecerliSifreKontrolu {
 
     public static void main(String[] args) {
 
+        // Kullanıcıdan giriş almak için Scanner nesnesi oluştur
         Scanner scanner = new Scanner(System.in);
 
+        // Kullanıcıya şifre belirlemesi için mesaj göster
         System.out.println("Şifrenizi belirleyiniz: ");
 
+        // Kullanıcının girdiği şifreyi oku
         String userpw = scanner.nextLine();
 
-        if (userpw.length() < 8){
+        // Şifre uzunluğu 8 karakterden az ise, şifre geçersizdir
+        if (userpw.length() < 8) {
             System.out.println("Geçersiz Şifre");
-        }else if (userpw.contains(" ")) {
+        }
+        // Şifre içinde boşluk karakteri varsa, şifre geçersizdir
+        else if (userpw.contains(" ")) {
             System.out.println("Geçersiz Şifre");
-        }else if (Character.isLowerCase(userpw.charAt(0))){
+        }
+        // Şifrenin ilk karakteri küçük harf ise, şifre geçersizdir
+        else if (Character.isLowerCase(userpw.charAt(0))) {
             System.out.println("Geçersiz Şifre");
-        } else if (userpw.endsWith("?")) {
+        }
+        // Şifre soru işareti '?' ile bitmiyorsa, şifre geçersizdir
+        else if (!userpw.endsWith("?")) {
             System.out.println("Geçersiz Şifre");
-        } else System.out.println("Geçerli Şifre");
+        }
+        // Yukarıdaki şartların hiçbiri sağlanmıyorsa, şifre geçerlidir
+        else {
+            System.out.println("Geçerli Şifre");
+        }
 
+        // Scanner nesnesini kapatarak kaynakları serbest bırak
         scanner.close();
-
     }
-
 }
