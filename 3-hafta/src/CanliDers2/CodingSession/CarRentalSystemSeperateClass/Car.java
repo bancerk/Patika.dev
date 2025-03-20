c418622d5cd3e922e3496cc203cf9abbd9f33d51
bancerk <192424850+bancerk@users.noreply.github.com>
2025-03-20 22:20:03 +0300

    week 3 live class 2

---
+++ 3-hafta/src/CanliDers2/CodingSession/CarRentalSystemSeperateClass/Car.java
@@ -0,0 +1,29 @@
+package CanliDers2.CodingSession.CarRentalSystemSeperateClass;
+
+// @Entity
+public class Car {
+
+    private String plate;
+    private boolean rented;
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public boolean isRented() {
+        return rented;
+    }
+
+    public void setRented(boolean rented) {
+        this.rented = rented;
+    }
+
+    @Override
+    public String toString() {
+        return getPlate() + " palakalı araç";
+    }
+}
--- 3-hafta/src/Odevler/BirbirineEnYakinİkiSayiyiBulma/NearestIntegers.java
+++ 3-hafta/src/Odevler/BirbirineEnYakinİkiSayiyiBulma/NearestIntegers.java
@@ -1,9 +1,40 @@
 package Odevler.BirbirineEnYakinİkiSayiyiBulma;
 
-import java.awt.*;
+import java.util.*;
 
 public class NearestIntegers {
 
-    List Integers = new List();
+    public static void main(String[] args) {
 
+        // Sayıları saklamak için bir liste oluşturuluyor
+        List<Integer> integers = new ArrayList<>();
+
+        // Listeye örnek sayılar ekleniyor
+        integers.add(54);
+        integers.add(48);
+        integers.add(64);
+        integers.add(33);
+        integers.add(72);
+        integers.add(96);
+        integers.add(12);
+        integers.add(23);
+        integers.add(7);
+        integers.add(88);
+
+        // küçükten büyüğe sıralamak için TreeSet’e çeviriyoruz
+        TreeSet<Integer> integerTreeSet = new TreeSet<>();
+
+        // Listedeki tüm elemanları TreeSet'e aktarıyoruz
+        integerTreeSet.addAll(integers);
+
+        System.out.println("Tree Set: " + integerTreeSet);
+
+
+    }
 }
+
+/*
+Integer veri tipinde bir ArrayList oluşturunuz
+ve içerisine 10 adet benzersiz ve pozitif eleman ekleyiniz.
+Bu listteki elemanlardan birbirine en yakın iki elamanı veren kodu yazınız.
+*/
