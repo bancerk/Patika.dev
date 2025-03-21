c93edf2b54ec55b920b7691b465c9a96938ffa01
bancerk <192424850+bancerk@users.noreply.github.com>
2025-03-21 22:56:38 +0300

    homework print map elements upload

--- 3-hafta/src/Odevler/PrintMapElements.java
+++ 3-hafta/src/Odevler/PrintMapElements.java
@@ -7,19 +7,20 @@ public class PrintMapElements {
 
     public static void main(String[] args) {
 
+        // Öğrencilerin isimlerini ve notlarını tutacak bir HashMap oluşturuluyor
         Map<String, Integer> scores = new HashMap<>();
 
+        // Map içerisine veriler ekleniyor
         scores.put("Ali", 85);
         scores.put("Ayşe", 92);
         scores.put("Mehmet", 78);
         scores.put("Zeynep", 90);
         scores.put("Burak", 88);
 
-        for (int i = 0; i <= scores.size(); i++){
-
-            System.out.println(i);
+        // Map’in her bir elemanını (anahtar-değer çiftini) ekrana yazdırıyoruz
+        for (Map.Entry<String, Integer> entry : scores.entrySet()) {
+            // entry.getKey() ile anahtarı (ismi), entry.getValue() ile değeri (notu) alıyoruz
+            System.out.println(entry.getKey() + " = " + entry.getValue());
         }
-
     }
-
 }
