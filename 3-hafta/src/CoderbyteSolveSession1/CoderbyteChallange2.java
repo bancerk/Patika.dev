c0ba41a5a8399ee57548893f8e6fc1b82da09606
bancerk <192424850+bancerk@users.noreply.github.com>
2025-03-21 16:32:33 +0300

    coderbyte challange solve session

---
+++ 3-hafta/src/CoderbyteSolveSession1/CoderbyteChallange2.java
@@ -0,0 +1,49 @@
+package CoderbyteSolveSession1;
+
+import java.util.Scanner;
+
+public class CoderbyteChallange2 {
+
+    public static int PrimeMover (int target) throws IllegalArgumentException{
+
+        if (target < 1 || target > 10000){
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println("num: " + target);
+
+        int primeNumberCounter = 0;
+        int nthPrimeNumber = 0;
+
+        for (int i = 2; primeNumberCounter != target;i++){
+            if (isPrime(i)){
+                primeNumberCounter++;
+                nthPrimeNumber = i;
+            }
+        }
+
+        return nthPrimeNumber;
+    }
+
+    private static boolean isPrime(int number){
+
+        if (number <= 1){
+            return false;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println(PrimeMover(s.nextInt()));
+
+    }
+
+}
