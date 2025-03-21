0d5f5b93e16dae93aa18a1a75ca96c7ba1c684a1
bancerk <192424850+bancerk@users.noreply.github.com>
2025-03-21 15:56:53 +0300

    homework movie collection progressing

--- 3-hafta/src/Odevler/FilmVeKoleksiyonFiltreleme/Film.java
+++ 3-hafta/src/Odevler/FilmVeKoleksiyonFiltreleme/Film.java
@@ -7,10 +7,44 @@ public class Film {
     private String movGenre;
     private double imdbPt;
 
+    public String getMovName() {
+        return movName;
+    }
+
+    public void setMovName(String movName) {
+        this.movName = movName;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getMovGenre() {
+        return movGenre;
+    }
+
+    public void setMovGenre(String movGenre) {
+        this.movGenre = movGenre;
+    }
+
+    public double getImdbPt() {
+        return imdbPt;
+    }
+
+    public void setImdbPt(double imdbPt) {
+        this.imdbPt = imdbPt;
+    }
+
     public Film(String movName, int releaseYear, String movGenre, double imdbPt) {
         this.movName = movName;
         this.releaseYear = releaseYear;
         this.movGenre = movGenre;
         this.imdbPt = imdbPt;
+
+
     }
 }
