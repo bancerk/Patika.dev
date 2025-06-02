fcbb501b3b61ff7b2ee3a1392f1c2b6f015905e3
bancerk <192424850+bancerk@users.noreply.github.com>
2025-06-02 23:17:06 +0300

    Repo and controller done

--- 13-hafta/SpringBoot/src/main/java/dev/patika/spring/CustomerController.java
+++ 13-hafta/SpringBoot/src/main/java/dev/patika/spring/CustomerController.java
@@ -1,9 +1,8 @@
 package dev.patika.spring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -16,7 +15,32 @@ public class CustomerController {
     }
 
     @GetMapping
-    public Customer findById(@PathVariable("id") int id){
+    public Customer findById(@PathVariable("id") int id) {
         return this.customerRepo.findById(id).orElseThrow();
+    }
+
+    @PostMapping("/save")
+    public Customer save(@RequestBody Customer customer) {
+        return this.customerRepo.save(customer);
+    }
+
+    @GetMapping("/find-all")
+    public List<Customer> findAll() {
+        return this.customerRepo.findAll();
+    }
+
+    @GetMapping("/find/{mail}")
+    public Customer findByMail(@PathVariable("mail") String mail) {
+        return this.customerRepo.findByMail(mail);
+    }
+
+    @GetMapping("/find/{mail}/{gender}")
+    public Customer findByMailAndGender(@PathVariable("mail") String mail, @PathVariable("gender") Customer.GENDER gender) {
+        return this.customerRepo.findByMailAndGender(mail, gender);
+    }
+
+    @GetMapping("/find/{gender}")
+    public List<Customer> findByGender(@PathVariable("gender") Customer.GENDER gender) {
+        return this.customerRepo.findByGender(gender);
     }
 }
